ec32ee5e5ba585bd848cf904d956d49eb8cbcd13
Vittor <vittorfranceschi04@gmail.com>
2025-09-29 15:18:52 -0300

    Login form recebendo token

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -5,39 +5,45 @@ function LoginForm() {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
 
-  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(`
-            Login feito, Credenciais:
-            ${email}
-            -------------
-            ${password}
-        `);
+    const response = await fetch("https://api-test-6v8d.onrender.com/tokens", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (data) localStorage.setItem("token", data.token);
   };
 
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleLogin} className={styles.form}>
       <h1>Bem vindo</h1>
       <p>Por favor, faça o login em sua conta e comece a gerenciar.</p>
-      <form onSubmit={handleLogin}>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          placeholder="Digite seu email."
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="password">Senha</label>
-        <input
-          type="password"
-          name="password"
-          placeholder="Digite sua senha."
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Logar</button>
-      </form>
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        name="email"
+        placeholder="Digite seu email."
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label htmlFor="password">Senha</label>
+      <input
+        type="password"
+        name="password"
+        placeholder="Digite sua senha."
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Logar</button>
     </form>
   );
 }
